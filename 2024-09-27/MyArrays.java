febf8b2844052e2e5d562691693c8ab15a2806d2
Hugo Hu <hugo.y.hu@hotmail.com>
2024-09-27 23:10:30 -0400

    add test case and concat code

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -49,6 +49,30 @@ public class MyArrays {
       "\nRun: " + arrayToString(concatArray(testArray1, testArray2))
     );
 
+    testArray1 = new int[] {};
+    testArray2 = new int[] {};
+
+    System.out.println(
+      "Expected: " + arrayToString(new int[] {}) +
+      "\nRun: " + arrayToString(concatArray(testArray1, testArray2))
+    );
+
+    testArray1 = new int[] {3, -1};
+    testArray2 = new int[] {};
+
+    System.out.println(
+      "Expected: " + arrayToString(new int[] {3, -1}) +
+      "\nRun: " + arrayToString(concatArray(testArray1, testArray2))
+    );
+
+    testArray1 = new int[] {};
+    testArray2 = new int[] {0};
+
+    System.out.println(
+      "Expected: " + arrayToString(new int[] {0}) +
+      "\nRun: " + arrayToString(concatArray(testArray1, testArray2))
+    );
+
   }
 
   public static String arrayToString(int[] nums) {
@@ -76,4 +100,17 @@ public class MyArrays {
     }
     return newAry;
   }
+
+  public static int[] concatArray(int[]ary1, int[]ary2) {
+    int[] newArray = new int[ary1.length + ary2.length];
+    for (int i = 0; i < ary1.length; i++) {
+      newArray[i] = ary1[i];
+    }
+    for (int i = 0; i < ary2.length; i++) {
+      newArray[i + ary1.length] = ary2[i];
+    }
+    return newArray;
+  }
+
+  
 }
