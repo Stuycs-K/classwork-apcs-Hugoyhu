f8696983c0b33211ca41511652024a03a8ef13c8
Hugo Hu <hugo.y.hu@hotmail.com>
2024-11-18 20:40:21 -0500

    add good case for part 2, day 1

--- 2024-11-18-advent2016/day1/part2/Solver.java
+++ 2024-11-18-advent2016/day1/part2/Solver.java
@@ -4,6 +4,9 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 public class Solver {
+
+
+
   public static void main (String[] args) {
     try {
       File file = new File("input.txt");
@@ -33,30 +36,58 @@ public class Solver {
         String remain = current.substring(1);
         int num = Integer.valueOf(remain);
 
+        int previousX = horDist;
+        int previousY = verDist;
+
+        coordinateList.add(new int[]{previousX, previousY});
+        boolean verticalChange = false;
+
         if (dir == 1) {
           verDist += num;
+          verticalChange = true;
         } else if (dir == 2) {
           horDist -= num;
         } else if (dir == 3) {
           verDist -= num;
+          verticalChange = true;
         } else {
           horDist += num;
         }
 
-        boolean found = false;
-        for (int j = 0; j < coordinateList.size(); j++) {
-            if ((horDist == coordinateList.get(j)[0]) && (verDist == coordinateList.get(j)[1])) {
-                
-                found = true;
+        if (coordinateList.size() > 1) {
+          for (int j = 1; j < coordinateList.size() - 1; j++) {
+            int test1X = coordinateList.get(j-1)[0];
+            int test1Y = coordinateList.get(j-1)[1];
+            int test2X = coordinateList.get(j)[0];
+            int test2Y = coordinateList.get(j)[1];
+
+            if ((test2X - test1X == 0) && (!verticalChange)) {
+              // vertical change on test, horizontal on recent
+              if ((Math.max(test2Y, test1Y) >= previousY) && (Math.min(test2Y, test1Y) <= previousY) && 
+              (Math.max(previousX, horDist) >= test1X) && (Math.min(previousX, horDist) <= test1X)
+              ) {
+                System.out.println("Intersect: " + test1X + " " + previousY);
+                break;
+              }
+
             }
+
+            if ((test2Y - test1Y == 0) && (verticalChange)) {
+              // vertical change on recent, horizontal on test
+              if ((Math.max(test2X, test1X) >= previousX) && (Math.min(test2X, test1X) <= previousX) && 
+              (Math.max(previousY, verDist) >= test1Y) && (Math.min(previousY, verDist) <= test1Y)
+              ) {
+                System.out.println("Intersect: " + previousX + " " + test1Y);
+                break;
+              }
+
+            }
+          }
         }
 
-        if (found) {
-            System.out.println(horDist + ", " + verDist);
-            break;
-        } else {
-            coordinateList.add(new int[]{horDist, verDist});
-        }
+        
+
+        
       }
 
       System.out.println(coordinateList);
