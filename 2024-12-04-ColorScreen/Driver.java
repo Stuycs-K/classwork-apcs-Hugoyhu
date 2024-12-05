db28799fbeb70974ccb6a7e8e686eda78a829c8a
Hugo Hu <hugo.y.hu@hotmail.com>
2024-12-04 20:12:21 -0500

    add

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -8,20 +8,33 @@ public class Driver {
 
         Text.color(Text.background(color));
 
-        Text.go(0,0);
+        // Text.go(0,0);
 
-        for (int i = 0; i < 30; i++) {
-            for (int j = 0; j < 80; j++) {
-                System.out.print(" ");
-            }
-        }
-
-        for (int i = 1; i < 29; i++) {
-            Text.erase(i+1, 2, 78);
-        }
+        // for (int i = 0; i < 30; i++) {
+        //     for (int j = 0; j < 80; j++) {
+        //         System.out.print(" ");
+        //     }
+        // }
 
         Text.color(Text.background(color));
-        Text.go(30, 0);
+        Text.go(1, 1);
+        for (int j = 0; j < 80; j++) {
+            System.out.print(" ");
+        }
+
+        for (int i = 2; i < 30; i++) {
+            Text.go(i, 1);
+            System.out.print(" ");
+            Text.go(i, 80);
+            System.out.print(" ");
+        }
+
+        // for (int i = 1; i < 29; i++) {
+        //     Text.erase(i+1, 2, 78);
+        // }
+
+        Text.color(Text.background(color));
+        Text.go(30, 1);
         for (int j = 0; j < 80; j++) {
             System.out.print(" ");
         }
@@ -41,10 +54,30 @@ public class Driver {
         return l;
     }
 
-    public static void main(String[] args) {
-        drawBorder(Text.CYAN);
+    public static void print3 (int[] args) {
+        int n0 = args[0];
+        int n1 = args[1];
+        int n2 = args[2];
 
-        int[] test = random3();
-        System.out.println(test[0] + " " + test[1] + " " + test[2]);
+        Text.go(1, 18);
+        System.out.print(n0);
+
+        Text.go(1, 38);
+        System.out.print(n1);
+
+        Text.go(1, 58);
+        System.out.print(n2);
+
+        System.out.print(Text.RESET);
+
+    }
+
+    public static void main(String[] args) {
+
+        int[] out3 = random3();
+        // System.out.println(test[0] + " " + test[1] + " " + test[2]);
+        print3(out3);
+
+        //drawBorder(Text.CYAN);
     }
 }
