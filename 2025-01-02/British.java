54a58761159b89973ebb03c4ec949b2341a38077
Hugo Hu <hugo.y.hu@hotmail.com>
2025-01-03 11:41:45 -0500

    add

---
+++ 2025-01-02/British.java
@@ -0,0 +1,19 @@
+public class British {
+
+    public static String[] numbers = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve"};
+    
+    
+    public static void main(String[] args) {
+        
+    }
+
+    public static void thousand (int number, String partial, int power) {
+        if (number < 13) {
+            partial += numbers[number];
+        } else {
+
+        }
+    }
+
+
+}
---
+++ 2025-01-02/MakeWords.java
@@ -0,0 +1,15 @@
+public class MakeWords {
+    public static void main(String[] args) {
+        makeWords(2,"","abc");
+    }
+
+    public static void makeWords(int remainingLetters, String result, String alphabet){
+        if (remainingLetters > 0) {
+            for (int i = 0; i < alphabet.length(); i++) {
+                makeWords (remainingLetters - 1, result + alphabet.charAt(i), alphabet);
+            }
+        } else {
+            System.out.println(result);
+        }
+    }
+}
